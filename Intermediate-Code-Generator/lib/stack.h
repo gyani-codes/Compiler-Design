4fcd5caed8c13b9c6e592b29dd4c1e702048ad65
Omkar Khodwe <bim2017007@iiita.ac.in>
2020-04-27 10:57:51 +0530

    Changes to stack files

--- Intermediate-Code-Generator/lib/stack.h
+++ Intermediate-Code-Generator/lib/stack.h
@@ -2,10 +2,10 @@
 #ifndef _STACK
 #define _STACK
 
-#define STACK_VAR_LENGTH 100
-#define STACK_CAPACITY 100
+#define BUFFER_TOTAL_CAPACITY 100
+#define STACK_VARIABLE_SIZE 100
+#define STACK_SIZE 100
 #define TEMP_VAR "#REG"
-#define BUFFER_STACK_CAPACITY 100
 
 typedef struct stack_node stack_node;
 typedef struct stack stack;
@@ -14,32 +14,32 @@ typedef struct TAC_code_stack TAC_code_stack;
 
 struct stack_node {
     int temp_num; // -1 if it isn't a temporary variable
-    char var_name[STACK_VAR_LENGTH];
+    char var_name[STACK_VARIABLE_SIZE];
 };
 
 struct stack {
     int top;
     int temp_count;
-    stack_node stack_arr[STACK_CAPACITY];
+    stack_node stack_arr[STACK_SIZE];
 };
 
 struct TAC_buffer_node {
-    char code[STACK_VAR_LENGTH];
+    char code[STACK_VARIABLE_SIZE];
     struct TAC_buffer_node *next;
 };
 
 struct TAC_code_stack {
     int top;
-    TAC_buffer_node *stack_arr[BUFFER_STACK_CAPACITY];
+    TAC_buffer_node *stack_arr[BUFFER_TOTAL_CAPACITY];
 };
 
-stack *initialize_stack();
-void free_stack(stack *st);
-stack_node pop_stack(stack *st);
-void print_stack_top(stack *st, TAC_buffer_node *buf);
-void push_stack(stack *st, const char *var_name);
+stack *init_s();
+void f_s(stack *st);
+stack_node popStack(stack *st);
+void print_st(stack *st, TAC_buffer_node *buf);
+void pushStack(stack *st, const char *var_name);
 
-TAC_code_stack *initialize_code_stack();
+TAC_code_stack *init_code_stack();
 TAC_buffer_node *get_new_node(TAC_code_stack *st);
 void print_TAC_code(TAC_code_stack *st);
 
